8765e7984bbea790c98d4c0b2e84baa8c41b068f
vinmean <vinmean@users.noreply.github.com>
2015-01-09 11:17:36 -0600

    Carousel started working

--- public/controllers/client-main.js
+++ public/controllers/client-main.js
@@ -1,6 +1,7 @@
 angular.module('EmpApp')
   .controller('MainCtrl', ['$scope', 'Employee', function ($scope, Employee) {
         $scope.headingTitle = 'First Ten Employees...';
+        $scope.slideInterval = 3000;
         $scope.employees = Employee.query();
 
   }]);
