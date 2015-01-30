2efa3ef228b20e7060de4e6c53244f1e0c25b55d
vinmean <vinmean@users.noreply.github.com>
2015-01-30 14:59:28 -0600

    Fixed canvas draw

--- public/js/ui-dom.js
+++ public/js/ui-dom.js
@@ -1,13 +1,26 @@
 ﻿function drawPhoto(canvasId,img, width, height){
     var canvas = document.getElementById(canvasId);
     var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
     var x = 0;
     var y = 0;
+    console.log('img.width = ' + img.width);
+    console.log('img.height = ' + img.height);
+
     if (img.width < width) {
         x = (width - img.width) / 2;
+        width = img.width;
     }
+
     if (img.height < height) {
         y = (height - img.height) / 2;
+        height = img.height
     }
+    
+    console.log('x = ' + x);
+    console.log('y = ' + y);
+    console.log('width = ' + width);
+    console.log('height = ' + height);
     context.drawImage(img, x, y, width, height);
 }
